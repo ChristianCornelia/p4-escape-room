fb4264bc9897cd0b58f187b9bb3bad1ec2141244
simionvw <61330022+simionvw@users.noreply.github.com>
2026-05-28 16:35:22 +0200

    yayayay homepage bijna klaar

--- index.php
+++ index.php
@@ -5,28 +5,22 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Escape Room</title>
-  <link rel="stylesheet" href="./css/style.css">
-  <style>
-    body {
-      background-image: url("images/background_cropped.png");
-      background-repeat: no-repeat;
-      background-attachment: fixed;
-      background-size: 100% 100%;
-    }
-  </style>
+  <link rel="stylesheet" href="./css/home/style.css">
 </head>
 
 <body>
-  <h1>Welkom</h1>
-  <p>De button hieronder is alleen voor testdoeleinden bij het opstarten van het project. Deze pagina is bedoeld voor de
-    uitleg van jullie escape room.</p>
-  <p>Via deze pagina gaat een team naar de aanmeldpagina</p>
+<nav>
+  <h1>Devil's Acre</h1>
+</nav>
 
+  <div class="div1">
+    <img src="images/parchment.png" class="img1">
+    <h1 class="maintext">Welcome to Devil's Acre, the most dangerous slum in 1800s London...</h1>
+    <a href="./rooms/room_1.php" class="buttons">Enter ye...</a>
+    <a href="./rooms/room_2.php" class="buttons">Enter ye...</a>
+  </div>
 
-  <button><a href="./rooms/room_1.php">Klik hier voor een demonstratie van kamer
-      1</a></button>
-  <button><a href="./rooms/room_2.php">Klik hier voor een demonstratie van kamer
-      2</a></button>
+  
 
 </body>
 
