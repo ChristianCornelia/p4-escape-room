4e856f6ef163a058fec55b09cb1362c85277ef6d
simionvw <61330022+simionvw@users.noreply.github.com>
2026-05-30 15:17:11 +0200

    help page

---
+++ help.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Escape Room</title>
+  <link rel="stylesheet" href="./css/home/style.css">
+</head>
+
+<body>
+<nav>
+  <h1>Devil's Acre</h1>
+  <a href="./index.php" class="login-btn">Home</a>
+  <a href="./login.php" class="login-btn">Login</a>
+</nav>
+
+  <div class="div1">
+    <img src="images/parchment.png" class="img1">
+    <h1 class="maintext">Welcome to Devil's Acre, the most dangerous slum in 1800s London...</h1>
+    <a class="helptext">In Devil's Acre you will first get stuck in an abandoned hospital, where you will have to try to escape unharmed... After that, you will end up in a sticky situation with some mafia members, and end up kidnapped in the basement of the mafia boss's complex where you will have to disguise yourself and trick the members to reach the top, where the boss resides, to stop the grip he has on the city once and for all...</a>
+  </div>
+
+  
+
+</body>
+
+</html>
