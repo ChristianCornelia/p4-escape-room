0f66b41f4253bf72c3aefc635c398650f228a100
Christian Cornelia <9028425@student.zadkine.nl>
2026-06-01 09:55:51 +0200

    win lose scherm

---
+++ lose.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Win scherm</title>
+      <link rel="stylesheet" href="./css/winlose/style.css">
+</head>
+<body class="lose">
+<nav>
+  <h1>Devil's Acre</h1>
+</nav>
+
+    <main>
+        <div class=winscreen>
+        <img class="blood" src="./images/blood.png" alt="">
+        <img class="cross" src="./images/cross.png" alt="">
+        <h1 class="losetext">Helaas, u heeft veloren.</h1>
+        <form class="toscore" action="scores.php">
+        <button >Naar scores</button>
+        </form>
+        </div>
+    </main>
+</body>
+</html>
